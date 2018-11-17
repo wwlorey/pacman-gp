b871560e778999f5ae5463030d5eacfda25f20e5
William Lorey <wwlorey@gmail.com>
2018-11-17 12:46:04 -0600

    Add fitness graph generation

--- analysis/gen_fitness_graphs.py
+++ analysis/gen_fitness_graphs.py
@@ -1,65 +1,79 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
 
 log_file_paths = \
     [
-        '../output/small_log.txt',
-        '../output/med_small_log.txt',
-        '../output/med_large_log.txt',
-        '../output/large_log.txt'
+        '../output/small_log.txt'
     ]
+'''
+'../output/large_log.txt',
+'../output/BONUS_small_log.txt',
+'../output/BONUS_small_competing_log.txt'
+'''
 
-NUM_EVALS_PERFORMED = 2000
+ARBITRARY_LARGE_NUMBER = 99999
+NUM_EVALS = 2000
 
-for file_index in range(len(log_file_paths)):
-    with open(log_file_paths[file_index], 'r') as log_file:
-        # Create a list of lines from the log file, disregarding all config parameters and empty lines
+for log_file_index in range(len(log_file_paths)):
+    with open(log_file_paths[log_file_index], 'r') as log_file:
+        # Create a list of lines from the log file, disregarding all config parmeters and empty lines
         log_file = log_file.read().split('\n')
         log_file = [line for line in log_file[log_file.index('Run 1'):] if not line == '']
 
-        # Get the index of each run header
-        run_header_indices = []
-        for index, line in enumerate(log_file):
-            if 'Run' in line:
-                run_header_indices.append(index)
+        # key: evaluation number, value: [average fitness, local best fitness]
+        eval_dict = {}
 
-        # Determine the last best score
-        last_best_score_run_index = 0
-        best_score = 0
-        for i, run_header_index in enumerate(run_header_indices[1:] + [len(log_file)]):
-            last_line_index = run_header_index - 1
+        curr_run = 1
 
-            last_score = int(log_file[last_line_index].split('\t')[1])
+        # Scrape data from the log file
+        for line in log_file:
+            if not line[0] == 'R':
+                # This line has eval and fitness data
+                eval_num, avg_fit, best_fit = line.split('\t')
 
-            if last_score > best_score:
-                best_score = last_score
-                last_best_score_run_index = i
+                eval_num = int(eval_num)
+                avg_fit = float(avg_fit)
+                best_fit = float(best_fit)
 
-        # Get data for the run containing the last best score
-        log_file = log_file[run_header_indices[last_best_score_run_index]:run_header_indices[last_best_score_run_index + 1]]
+                if eval_num in eval_dict:
+                    eval_dict[eval_num][0] += avg_fit
+                    eval_dict[eval_num][1] += best_fit
+                    eval_dict[eval_num][2] += 1
+                    eval_dict[eval_num][3] = min(eval_dict[eval_num][3], avg_fit)
+                else:
+                    eval_dict[eval_num] = [avg_fit, best_fit, 1, ARBITRARY_LARGE_NUMBER]
 
-        # Get evals and fitnesses for the best run
         evals = []
-        fits = []
+        avg_fits = []
+        best_fits = []
+        worst_fits = []
+        for eval_num in sorted(eval_dict.keys()):
+            evals.append(eval_num)
+            avg_fits.append(eval_dict[eval_num][0] / eval_dict[eval_num][2])
+            best_fits.append(eval_dict[eval_num][1] / eval_dict[eval_num][2])
+            worst_fits.append(eval_dict[eval_num][3] / eval_dict[eval_num][2])
 
-        for line in log_file[1:]:
-            eval_num, fitness = line.split('\t')
-            evals.append(int(eval_num))
-            fits.append(int(fitness))
 
-        # Adjust evals and fitnesses to be the correct length
-        evals += [NUM_EVALS_PERFORMED]
-        fits += [fits[-1]]
+        data = []
+        for i in range(len(avg_fits)):
+            data.append([worst_fits[i], avg_fits[i], best_fits[i]])
 
         # Plot the results
-        plt.step(evals, fits, '-b')
+        fig, ax = plt.subplots()
 
-        # Include necessary labels
+        ax.boxplot(data)
+
+        plt.xticks(np.arange(len(evals) + 2), tuple([0] + evals[2::4]))
+
+        ax.set_xticks(ax.get_xticks()[::4])
+    
+        # Include labels
         plt.xlabel('Evaluations')
         plt.ylabel('Fitness')
 
         # Save and close the plot
-        plt.savefig(log_file_paths[file_index][:log_file_paths[file_index].find('log')] + 'graph.png')
+        plt.savefig(log_file_paths[log_file_index][:log_file_paths[log_file_index].find('.txt')] + '_graph.png')
         plt.close()
-
