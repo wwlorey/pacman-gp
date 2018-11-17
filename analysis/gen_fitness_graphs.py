2e9e0fd4bbb2f6612071d034aaf5d6051e5984e7
William Lorey <wwlorey@gmail.com>
2018-11-17 16:16:27 -0600

    Add BONUS1 files

--- analysis/gen_fitness_graphs.py
+++ analysis/gen_fitness_graphs.py
@@ -7,10 +7,10 @@ import numpy as np
 log_file_paths = \
     [
         '../output/small_log.txt',
-        '../output/large_log.txt'
+        '../output/large_log.txt',
+        '../output/BONUS_small_log.txt'
     ]
 '''
-'../output/BONUS_small_log.txt',
 '../output/BONUS_small_competing_log.txt'
 '''
 
